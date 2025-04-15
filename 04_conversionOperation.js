b7e8356046828745fb1869e6ad0cdf3ee092312e
Muhammadumairramzan <78447194+Muhammadumairramzan@users.noreply.github.com>
2025-04-15 19:54:04 +0000

    04_conversionOperation

---
+++ 04_conversionOperation.js
@@ -0,0 +1,31 @@
+// For Negitive value
+
+let value = 3
+let neg_value = -value
+console.log(neg_value);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);   bad approach 
+// console.log("1" + 2 + "2");
+// console.log(typeof true)
+
+let str1 = "Umair"
+let str2 = " Ramzan"
+let str3 = str1+str2
+console.log(str3)
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+console.log(num1)
+
+let game_counter = 100
+game_counter++;
+console.log(game_counter);
