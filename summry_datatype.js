706fd3d51f25ae2a61046a6727836137a1990def
Muhammadumairramzan <78447194+Muhammadumairramzan@users.noreply.github.com>
2025-04-15 22:45:00 +0000

    summry_datatypes

---
+++ summry_datatype.js
@@ -0,0 +1,21 @@
+// primitive data types
+// String, Number, Boolean, null,undefined,symbol,bigInt,
+// Non-Primtive/Refrence Types
+// Array,Objects,Functions
+const score = 100
+const score_value = 100.2
+const is_logged_in = false
+const outside_temp = null
+let user_email;
+const id= Symbol('123')
+const big_number = 3232323234343343n
+const new_var = ['umair','ramzan',295];
+let myobj = {
+    name:"umair",
+    age:22,
+}
+const my_function = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof big_number)   //Big int example
