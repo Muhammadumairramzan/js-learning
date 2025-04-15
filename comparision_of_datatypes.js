86809a5d0c451c1f7b5c8f913677b3d5e6f7103f
Muhammadumairramzan <78447194+Muhammadumairramzan@users.noreply.github.com>
2025-04-15 20:43:35 +0000

    Comparison of datatypes

---
+++ comparision_of_datatypes.js
@@ -0,0 +1,9 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("22" == 22) //it will only check the value 
+
+console.log("23" === 22) //it will  check both value and data type
