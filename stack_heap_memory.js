55ac1f168efe7aed1543dc596f591fe90baae1d1
Muhammadumairramzan <78447194+Muhammadumairramzan@users.noreply.github.com>
2025-04-16 16:54:28 +0000

    stack_heap_memory

---
+++ stack_heap_memory.js
@@ -0,0 +1,19 @@
+// stack memory  (Primtive datatypes)
+// heap  memory  (Non-Primtive datatypes)
+//For example
+// let my_name = "mynameisumair"
+// let another_name = "myNmame"
+// another_name = "saad"
+// console.log(my_name)
+// console.log(another_name)
+
+let user_one = {
+    email: "umair@google.com",
+    phone:  3016104788
+}
+
+let use_two = {
+    email: "umairramzan@selectasol.com"
+}
+console.log(user_one.phone)
+console.log(use_two.email)
