1f35ddc6c55ec567d212e98f535c2680b7c81d21
Muhammadumairramzan <78447194+Muhammadumairramzan@users.noreply.github.com>
2025-04-17 16:21:34 +0000

    string

---
+++ strings.js
@@ -0,0 +1,14 @@
+const name = "umair";
+const repo = 50;
+
+console.log(`hello my is ${name.toUpperCase()}`);
+
+
+const gameName = new String('umair')
+console.log(gameName[0]);
+console.log(gameName._proto_);
+console.log(gameName.length);
+console.log(gameName.toLocaleLowerCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf("a"));
+console.log(gameName.substring("a"));
