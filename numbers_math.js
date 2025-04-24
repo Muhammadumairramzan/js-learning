db5210a4e81959570e1c8da53e6baaa24f98149b
Muhammadumairramzan <78447194+Muhammadumairramzan@users.noreply.github.com>
2025-04-24 22:14:45 +0000

    07_numbers

---
+++ numbers_math.js
@@ -0,0 +1,11 @@
+const number = 400;
+console.log(number)
+const new_number = 200;
+console.log(new_number)
+
+console.log(new_number.toString().length)
+const balance = new Number(222)
+console.log(balance.toFixed(1))
+
+const other_number = 23.8933;
+console.log(other_number.toPrecision(4));
