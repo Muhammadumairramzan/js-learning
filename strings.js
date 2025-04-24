acbc771757364aef7e93a3789ac383e05c7200fc
Muhammadumairramzan <78447194+Muhammadumairramzan@users.noreply.github.com>
2025-04-24 22:06:06 +0000

    strings.js

--- strings.js
+++ strings.js
@@ -12,3 +12,13 @@ console.log(gameName.toLocaleLowerCase());
 console.log(gameName.charAt(3));
 console.log(gameName.indexOf("a"));
 console.log(gameName.substring("a"));
+
+const new_string_one = "   umair R   rr "
+console.log(new_string_one.trim())
+
+const url = "https://locallypages. com/"
+console.log(url.replace("s", "d"))
+
+console.log(url.includes(". com"))
+
+console.log(name.split('-'))
