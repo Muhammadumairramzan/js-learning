ae29e9c342744948489b9a045f6996d7acd12822
Muhammadumairramzan <78447194+Muhammadumairramzan@users.noreply.github.com>
2025-05-22 21:52:31 +0000

    10_pp_object.js

---
+++ 10_pp_object.js
@@ -0,0 +1,14 @@
+const course = {
+    course_name: "js_learning",
+    price: "999",
+    instructor: "umiar"
+}
+// console.log(course.course_name)
+const{course_name: xyz} = course
+console.log(xyz)
+
+{
+    name:"umair",
+    "course_name":"js",
+    "price":"free"
+}
