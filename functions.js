fa82cfaa6b303bde3f5a5b8266e0d9393691d786
Muhammadumairramzan <78447194+Muhammadumairramzan@users.noreply.github.com>
2025-05-28 22:41:32 +0000

    functions.js

---
+++ functions.js
@@ -0,0 +1,20 @@
+
+// Here three dots are working as rest operator
+function add_cart_price(val1,val2,...num1){
+    return num1
+}
+// console.log(add_cart_price(2,3,4,6)) 
+
+// const user = {
+//     user_name: "umair",
+//     price:199
+// }
+// function handle_object(anyobj){
+//     console.log(`user name is ${anyobj.user_name} and price is ${anyobj.price}.`);
+// }
+// handle_object(user)
+const myNewArray = [200,400,300,100]
+function retrunSecondValue(getArray){
+    return getArray[2]
+}
+console.log(retrunSecondValue(myNewArray))
